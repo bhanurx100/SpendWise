346b682a7cbc6ef0447492e3e38cbe8422930fe3
bhanurx100 <bhanu100rx@gmail.com>
2025-05-04 20:24:47 +0530

    deployed vercel

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,6 @@
 import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
 
-import { links } from "@/config";
 
 import { Filters } from "./filters";
 import { HeaderLogo } from "./header-logo";
@@ -29,14 +26,7 @@ export const Header = () => {
               <Loader2 className="size-8 animate-spin text-slate-400" />
             </ClerkLoading>
 
-            <Link
-              href={links.sourceCode}
-              target="_blank"
-              rel="noreferrer noopener"
-              title="Source Code"
-            >
-              <Image src="/github.svg" alt="GitHub" height={24} width={24} />
-            </Link>
+            
           </div>
         </div>
 
